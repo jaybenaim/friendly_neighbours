319355c6d497eadbf7d84e884a5a4963145d4e15
jaybenaim <benaimjacob@gmail.com>
2019-09-22 12:49:31 -0400

    build failing

--- friendly_neighbours/settings.py
+++ friendly_neighbours/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-FRIENDLY_NEIGHBOURS_FRONTEND = os.path.join(FRIENDLY_NEIGHBOURS_FRONTEND, 'friendly_neighbours_frontend')
+FRIENDLY_NEIGHBOURS_FRONTEND = os.path.join(BASE_DIR, 'friendly_neighbours_frontend')
 
 STATICFILES_DIRS = [
-    os.path.join(FRIENDLY_NEIGHBOURS_FRONTEND, 'build', 'static'),
+    os.path.join(BASE_DIR, 'build', 'static'),
 ]
