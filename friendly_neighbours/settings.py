95f21482bfb67d6ec6ba139fe190ad61c9dec0da
jaybenaim <benaimjacob@gmail.com>
2019-09-22 12:14:14 -0400

    running test success

--- friendly_neighbours/settings.py
+++ friendly_neighbours/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'friendly_neighbours.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'friendly_neighbours',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
